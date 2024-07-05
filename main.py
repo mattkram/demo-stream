7bb15fa890db13d6f7d5f0b2814d6b661bb7173a
Matt Kramer <matthew.robert.kramer@gmail.com>
2024-07-05 00:26:33 -0500

    feat: Add mypy (#12)
    * Add mypy
    
    * Fix types
    
    * Run type checks in CI

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.get("/healthz")
-async def health():
+async def health() -> dict[str, str]:
     return {"status": "ok"}
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,14 @@
-async def test_get_root(client):
+from httpx import AsyncClient
+
+
+async def test_get_root(client: AsyncClient) -> None:
     response = await client.get("/")
     assert response.status_code == 200
     data = response.json()
     assert data == {"message": "Hello World"}
 
 
-async def test_get_health(client):
+async def test_get_health(client: AsyncClient) -> None:
     response = await client.get("/healthz")
     assert response.status_code == 200
     data = response.json()
