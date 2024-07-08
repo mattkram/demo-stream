fb778e38e512d61fb93e6226df00ba310ade6189
mattkram <mkramer@anaconda.com>
2024-07-07 21:39:33 -0500

    Render a list of videos on the homepage

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import re
+from datetime import datetime
+from pathlib import Path
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+VIDEOS_DIR = Path(__file__).parent / "videos"
 
 app = FastAPI()
 
@@ -11,11 +18,49 @@ app.mount("/videos", StaticFiles(directory="videos"), name="videos")
 templates = Jinja2Templates(directory="templates")
 
 
+class Video(BaseModel):
+    timestamp: datetime
+    title: str
+    uri: str
+
+    @classmethod
+    def from_path(cls, path: Path) -> "Video":
+        filename = path.name
+        if m := re.match(
+            r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})",
+            filename,
+        ):
+            year = int(m.group("year"))
+            month = int(m.group("month"))
+            day = int(m.group("day"))
+            timestamp = datetime(year, month, day)
+        else:
+            raise ValueError(f"Could not match string {filename}")
+
+        return Video(
+            uri=f"/videos/{path.name}",
+            timestamp=timestamp,
+            title=(str(path.name)[11:].replace(".mp4", "").replace("-", " ").title()),
+        )
+
+
+def _load_videos() -> list[Video]:
+    return sorted(
+        (Video.from_path(p) for p in VIDEOS_DIR.glob("*.mp4")),
+        key=lambda v: v.timestamp,
+        reverse=True,
+    )
+
+
 @app.get("/")
 async def home(request: Request) -> HTMLResponse:
+    videos = _load_videos()
     return templates.TemplateResponse(
         request=request,
         name="home.html",
+        context={
+            "videos": videos,
+        },
     )
 
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,6 @@
 from httpx import AsyncClient
 
 
-async def test_get_root(client: AsyncClient) -> None:
-    response = await client.get("/")
-    assert response.status_code == 200
-    data = response.json()
-    assert data == {"message": "Hello World"}
-
-
 async def test_get_health(client: AsyncClient) -> None:
     response = await client.get("/healthz")
     assert response.status_code == 200
