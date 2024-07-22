cf8eb169bca614c9f787faac6a3205f81fc145e4
mattkram <mkramer@anaconda.com>
2024-07-22 13:14:05 -0500

    Remove videos stuff

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import functools
 import hashlib
 import json
-import re
 from contextvars import ContextVar
-from datetime import datetime
-from pathlib import Path
-from typing import Annotated, Any
+from typing import Annotated
 
 import jinja_partials
-from fastapi import FastAPI, Form, Header, Query, Request, Response
+from fastapi import FastAPI, Form, Header, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -17,7 +13,6 @@ from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoin
 
 templates = Jinja2Templates("tests/test_templates")
 
-VIDEOS_DIR = Path(__file__).parent / "videos"
 REQUEST_CTX_KEY = "request_id"
 
 _request_ctx_var: ContextVar[Request] = ContextVar(REQUEST_CTX_KEY, default=None)
@@ -25,7 +20,6 @@ _request_ctx_var: ContextVar[Request] = ContextVar(REQUEST_CTX_KEY, default=None
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/videos", StaticFiles(directory="videos"), name="videos")
 
 templates = Jinja2Templates(directory="templates")
 jinja_partials.register_starlette_extensions(templates)
@@ -49,78 +43,15 @@ class RequestContextMiddleware(BaseHTTPMiddleware):
 app.add_middleware(RequestContextMiddleware)
 
 
-class Video(BaseModel):
-    timestamp: datetime
-    title: str
-    uri: str
-
-    @classmethod
-    def from_path(cls, path: Path) -> "Video":
-        filename = path.name
-        if m := re.match(
-            r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})",
-            filename,
-        ):
-            year = int(m.group("year"))
-            month = int(m.group("month"))
-            day = int(m.group("day"))
-            timestamp = datetime(year, month, day)
-        else:
-            raise ValueError(f"Could not match string {filename}")
-
-        return Video(
-            uri=f"/videos/{path.name}",
-            timestamp=timestamp,
-            title=(str(path.name)[11:].replace(".mp4", "").replace("-", " ").title()),
-        )
-
-
-class VideosListViewModel(BaseModel):
-    videos: list[Video]
-
-
-def _load_videos(order: str) -> list[Video]:
-    return sorted(
-        (Video.from_path(p) for p in VIDEOS_DIR.glob("*.mp4")),
-        key=lambda v: v.timestamp,
-        reverse=(order == "desc"),
-    )
-
-
 def render_template(name, model: BaseModel | None = None):
     context = {"model": model.dict(), **model.dict()} if model is not None else {}
     return templates.TemplateResponse(request=get_request(), name=name, context=context)
-
-
-def template(name: str) -> Any:
-    """A decorator to convert a view model object into an HTML response, via Jinja2 template."""
-
-    def decorator(f):
-        @functools.wraps(f)
-        async def inner(*args: Any, **kwargs: Any):
-            model = await f(*args, **kwargs)
-            return render_template(name, model)
-
-        return inner
-
-    return decorator
 
 
 @app.get("/")
 async def home() -> HTMLResponse:
     with open("templates/index.html", "rb") as fp:
         return HTMLResponse(content=fp.read(), media_type="text/html", status_code=200)
-
-
-@app.get("/videos")
-async def videos(
-    from_htmx: Annotated[str, Header(alias="hx-request")] = "",
-    order: Annotated[str, Query()] = "desc",
-) -> HTMLResponse:
-    model = VideosListViewModel(videos=_load_videos(order=order))
-    if from_htmx:
-        return render_template("partials/videos_list.html", model=model)
-    return render_template("home.html", model=model)
 
 
 @app.get("/healthz")
